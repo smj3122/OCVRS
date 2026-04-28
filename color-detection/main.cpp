4f2a6d1f87a3ac318632fc41df1da01a322aab73
pinkCastle2204 <samutkose@gmail.com>
2026-04-28 17:26:36 +0530

    Add Centroid and Implement EMA

--- color-detection/main.cpp
+++ color-detection/main.cpp
@@ -21,7 +21,13 @@ int main (){
     int hmin = 0, smin = 75, vmin = 0;
     int hmax = 5, smax = 255, vmax = 255;
 
-    int threshArea = 250;
+    int threshArea = 7000;
+
+    float smoothCx = 0;
+    float smoothCy = 0;
+
+    bool firstFrame = true;
+    float alpha  = 0.4f;
 
 
 
@@ -45,7 +51,7 @@ int main (){
     cv::createTrackbar("Saturation Max", "Sliders",&smax,255);
     cv::createTrackbar("Value Min", "Sliders",&vmin,255);
     cv::createTrackbar("Value Max", "Sliders",&vmax,255);
-    cv::createTrackbar("Area Threshold", "Sliders",&threshArea,1000);
+    cv::createTrackbar("Area Threshold", "Sliders",&threshArea,50000);
    /*
     cv::createTrackbar("Hue Min", "DEBUG",&hmin,179);
     cv::createTrackbar("Hue Max", "DEBUG",&hmax,179);
@@ -56,6 +62,9 @@ int main (){
     */
     cv::namedWindow("Morphologytest",cv::WINDOW_NORMAL);
     cv::resizeWindow("Morphologytest",scr_width,480);
+
+
+    //========Main WebCAM Loop
     
     while (video.isOpened()){
        
@@ -64,6 +73,9 @@ int main (){
         
         cv::flip(frame, frame, 1);
         cv::Mat matDebug = frame.clone();
+
+        cv::GaussianBlur(frame,frame,cv::Size(5,5),0);
+        
         cv::Scalar lim_lower(hmin, smin, vmin);
         cv::Scalar lim_upper(hmax, smax, vmax);
         cv::Mat ColoredObj;
@@ -77,20 +89,19 @@ int main (){
         cv::Mat OutputframeClosing{mask.size(),mask.type()};
 
         int kernel_size = 5;
-        //temp1 = erosion(mask,temp1,kernel_size);
-        //OutputframeOpening = dilation(temp1,OutputframeOpening,kernel_size);
-        temp1 = dilation(mask,temp1,kernel_size);
-        OutputframeClosing = erosion(temp1,OutputframeClosing,kernel_size);
-        temp1 = dilation(OutputframeClosing,temp1,kernel_size);
-        OutputframeClosing = erosion(temp1,OutputframeClosing,kernel_size);
 
+        temp1 = erosion(mask,temp1,kernel_size);
+        OutputframeOpening = dilation(temp1,OutputframeOpening,kernel_size);
+        temp1 = dilation(OutputframeOpening,temp1,kernel_size);
+        OutputframeClosing = erosion(temp1,OutputframeClosing,kernel_size);
+        
         cv::bitwise_and(matDebug,matDebug,ColoredObj,OutputframeClosing);
 
 
         //===============contours====================
         std::vector<std::vector<cv::Point>> contours;
         std::vector<cv::Vec4i> hierarchy;
-        cv::findContours(OutputframeClosing,contours, hierarchy,cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+        cv::findContours(OutputframeClosing,contours, hierarchy,cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
             for(size_t i = 0; i < contours.size(); i++){
                 double area = cv::contourArea(contours[i]);
@@ -98,9 +109,27 @@ int main (){
                 if(area > threshArea){
                     cv::drawContours(ColoredObj, contours, i, cv::Scalar(0,255,0),2);
                     cv::drawContours(matDebug, contours, i, cv::Scalar(0,255,0),2);
-                }       
 
- 
+                    cv::Moments M = cv::moments(contours[i]);
+                    if(M.m00 != 0){
+                        int cx = int(M.m10/ M.m00);
+                        int cy = int(M.m01 / M.m00);
+
+                        if(firstFrame){
+
+                            smoothCx = cx;
+                            smoothCy = cy;
+
+                            firstFrame = false;
+                        }
+
+                        smoothCx = alpha * cx + (1.0 - alpha) * smoothCx;
+                        smoothCy = alpha * cy + (1.0 - alpha) * smoothCy;
+
+                        cv::circle(ColoredObj, cv::Point(smoothCx,smoothCy),5,cv::Scalar(255,0,0), -1);
+                        cv::circle(matDebug, cv::Point(smoothCx,smoothCy),5,cv::Scalar(255,0,0), -1);
+                    }
+                }       
             }
         
 
